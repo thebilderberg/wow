bd0a152cfe2a39597a4c977c733423996bd36232
thebilderberg <mygooglegirls@yandex.ru>
2022-06-10 00:50:47 +0300

    vol.1

---
+++ js/enter.js
@@ -0,0 +1,20 @@
+"use strict";
+
+function holdMenu() {
+    let menu = document.querySelector('nav');
+    let iconB = document.querySelector('.iconBurger');
+    if (menu.classList.contains('hide')){
+        menu.classList.remove("hide");
+    }
+    else {
+        menu.classList.add("hide");
+
+    }
+    if (iconB.classList.contains('hide')){
+        iconB.classList.remove("hide");
+    }
+    else {
+        iconB.classList.add("hide");
+    }
+}
+
